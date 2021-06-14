0f51317e746d7f5e5c63ad249ea23b0abefa3aab
Joe2k <jonathansamuel2k@gmail.com>
2021-06-14 14:33:24 +0530

    Add commands

--- Game.java
+++ Game.java
@@ -1,8 +1,19 @@
 import src.*;
 
 public class Game {
+
+    private static boolean[][] board = new boolean[10][10];
     public static void main(String args[]) {
-        Input input = new Input();
-        input.getInput();
+        Display display = new Display();
+        display.displayBoard(board);
+        int choice = display.displayCommands();
+        int coordinates[];
+        switch (choice) {
+            case 1:
+                coordinates = display.getCoordinates();
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ src/Display.java
@@ -0,0 +1,51 @@
+package src;
+
+import java.util.*;
+
+public class Display {
+
+    public void displayBoard(boolean[][] board) {
+
+        System.out.println("Your Current Board \nO - Live Cell     X - Dead Cell \n");
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (board[i][j]) {
+                    System.out.print("O (" + Integer.toString(i) + ", " + Integer.toString(j) + ")  ");
+                } else {
+                    System.out.print("X (" + Integer.toString(i) + ", " + Integer.toString(j) + ")  ");
+                }
+
+            }
+            System.out.println("");
+        }
+    }
+    
+    public int displayCommands() {
+        System.out.println("Press 1 to select a cell and negate it's current state");
+        System.out.println("Press 2 to run the Conway's Game of Life");
+        System.out.println("Press any other character to exit the game");
+
+        Scanner myInput = new Scanner(System.in);
+        int choice = myInput.nextInt();
+        myInput.close();
+
+        return choice;
+    }
+
+    public int[] getCoordinates() {
+        int coordinates[] = new int[2];
+        Scanner myInput = new Scanner(System.in);
+
+        System.out.print("Enter the 1st co-ordinate of the cell : ");
+        coordinates[0] = myInput.nextInt();
+
+        System.out.print("Enter the 2nd co-ordinate of the cell : ");
+        coordinates[1] = myInput.nextInt();
+
+        myInput.close();
+        
+        return coordinates;
+    }
+    
+}
